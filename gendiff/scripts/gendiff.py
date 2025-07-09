9072e7136608fa87a52832a7b7453eda9ecac3f2
Jacobo Jaramillo <jacobo.jaramillo@hotmail.com>
2025-07-09 11:04:42 -0700

    Creacion File1 y File2, y lectura y carga de contenido en gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser(
@@ -8,6 +9,15 @@ def main():
     parser.add_argument("second_file", help="Second file to compare")
 
     args = parser.parse_args()
+
+    with open(args.first_file) as file1:
+        data1 = json.load(file1)
+
+    with open(args.second_file) as file2:
+        data2 = json.load(file2)
+    print('Contenido primer archivo', data1)
+    print('Contenido segundo archivo', data2)
+
 
 if __name__ == "__main__":
     main()
