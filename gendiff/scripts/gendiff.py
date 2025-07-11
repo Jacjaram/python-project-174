828a2bdc34717f7c41f2d6bbe7504335da43f234
Jacobo Jaramillo <jacobo.jaramillo@hotmail.com>
2025-07-11 10:16:36 -0700

    Correccion de lint, y creaccion makefile con make lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 
+
 def main():
     import sys
     sys.argv = ["gendiff.py", "file1.json", "file2.json"]
@@ -23,31 +24,26 @@ def generate_diff(file_path1, file_path2):
     with open(file_path2) as f2:
         file2 = json.load(f2)
 
-
     diff = {}
     agregados = set(file2.keys()) - set(file1.keys())
     eliminados = set(file1.keys()) - set(file2.keys())
     claves_comunes = set(file1.keys()) & set(file2.keys())
-    
 
     for i in claves_comunes:
         if file1[i] == file2[i]:
-            diff.update({i : file1[i]})
+            diff.update({i: file1[i]})
         else:
             eliminados.add(i)
             agregados.add(i)
 
-
-    diff.update({f'- {k}':file1[k] for k in eliminados if k in file1})    
-    diff.update({f'+ {k}':file2[k] for k in agregados if k in file2})
-    
+    diff.update({f'- {k}': file1[k] for k in eliminados if k in file1})
+    diff.update({f'+ {k}': file2[k] for k in agregados if k in file2})
 
     def clave_sin_prefijo(clave):
         return clave[2:] if clave.startswith(('+ ', '- ')) else clave
 
-
-    diff_ordenado = dict(sorted(diff.items(), key=lambda item: clave_sin_prefijo(item[0])))
-
+    diff_ordenado = dict(sorted(diff.items(),
+                                key=lambda item: clave_sin_prefijo(item[0])))
     lineas = ["{"]
     for clave, valor in diff_ordenado.items():
         prefijo = "  "
