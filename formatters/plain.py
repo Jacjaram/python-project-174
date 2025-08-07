88a33fa85b8f091d86fb26934e6dfc7b5225fa1a
Jacobo Jaramillo <jacobo.jaramillo@hotmail.com>
2025-08-07 09:45:50 -0700

    Value.py eliminado se incorpora el formato_value a cada funcion para tenerlas independientes

--- formatters/plain.py
+++ formatters/plain.py
@@ -1,4 +1,14 @@
-from formatters.value import format_value
+def format_value(value):    
+    if isinstance(value, (dict, list)):
+        return '[complex value]'
+    elif isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return "null"
+    elif isinstance(value, str):
+        return f"'{value}'"
+    else:
+        return str(value)
 
 def format_plain(diff, path=''):
     lines = []
@@ -9,11 +19,11 @@ def format_plain(diff, path=''):
         if status == 'nested':
             lines.append(format_plain(value['children'], new_path))
         elif status == 'changed':
-            lines.append(f"Property '{new_path}' was updated. From {format_value(value['old_value'], 1, 'plain')} to {format_value(value['new_value'], 1, 'plain')}")
+            lines.append(f"Property '{new_path}' was updated. From {format_value(value['old_value'])} to {format_value(value['new_value'])}")
         # elif status == 'unchanged':
         #     print('no cambió')
         elif status == 'added':
-            lines.append(f"Property '{new_path}' was added with value: {format_value(value['value'], 1, 'plain')}")
+            lines.append(f"Property '{new_path}' was added with value: {format_value(value['value'])}")
         elif status == 'removed':
             lines.append(f"Property '{new_path}' was removed")
 
--- formatters/stylish.py
+++ formatters/stylish.py
@@ -1,6 +1,18 @@
-from formatters.value import format_value
-
-
+def format_value(value, depth=1):
+    if isinstance(value, dict):
+        indent = '    ' * depth
+        lines = ['{']
+        for key, val in value.items():
+            lines.append(f'{indent}    {key}: {format_value(val, depth + 1)}')
+        lines.append(f'{indent}}}')
+        return '\n'.join(lines)
+    elif isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return "null"
+    else:
+        return str(value)
+    
 def format_stylish(diff, depth=1):
     indent = '    ' * (depth - 1)
     lines = ['{']
